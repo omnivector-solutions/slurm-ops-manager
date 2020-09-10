466ca7c114baa0140013f094c16f0e2dc018d999
jamesbeedy <jamesbeedy@users.noreply.github.com>
2020-09-10 11:08:35 -0700

    Back to bdx (#17)

--- slurm_ops_manager/__init__.py
+++ slurm_ops_manager/__init__.py
@@ -1,3 +1,1 @@
-from .slurm_snap_ops import * # noqa
-from .slurm_tar_ops import * # noqa
 from .slurm_ops import * # noqa
--- slurm_ops_manager/slurm_ops.py
+++ slurm_ops_manager/slurm_ops.py
@@ -1,137 +1,124 @@
-"""Charm class to install slurm via snap or tar resource."""
+#!/usr/bin/env python3
+"""This module provides the SlurmManager."""
 import logging
-import os
-import subprocess
 import tarfile
-from base64 import b64decode, b64encode
 from pathlib import Path
+from time import sleep
 
-from jinja2 import (
-    Environment,
-    FileSystemLoader,
+from ops.framework import (
+    Object,
+    StoredState,
 )
-from ops.framework import Object, StoredState
 from ops.model import ModelError
-from slurm_ops_manager.slurm_snap_ops import SlurmSnapManager
-from slurm_ops_manager.slurm_tar_ops import SlurmTarManager
+from slurm_ops_manager.slurm_ops_managers import (
+    SlurmSnapManager,
+    SlurmTarManager,
+)
+from slurm_ops_manager.utils import get_inventory
 
 
 logger = logging.getLogger()
 
 
-class SlurmOpsManager(Object):
-    """Config values to install slurm."""
+class SlurmManager(Object):
+    """SlurmOpsManager."""
 
     _stored = StoredState()
 
-    _TEMPLATE_DIR = Path(
-        os.path.dirname(
-            os.path.abspath(__file__)
-        )
-    ) / 'templates'
-
     def __init__(self, charm, component):
-        """Determine values based on resource type."""
+        """Set the initial attribute values."""
         super().__init__(charm, component)
-
+        self._stored.set_default(slurm_installed=False)
+        self._stored.set_default(slurm_version_set=False)
         self._stored.set_default(resource_path=None)
+        self._stored.set_default(resource_checked=False)
 
-        self.charm = charm
+        self._charm = charm
         self._slurm_component = component
-        self._is_tar = None
-        self._resource_path = None
-        try:
-            if not self._stored.resource_path:
+
+        if not self._stored.resource_checked:
+            try:
                 self._stored.resource_path = str(
                     self.model.resources.fetch('slurm')
                 )
-            self._resource_path = Path(self._stored.resource_path)
+            except ModelError as e:
+                logger.debug(e)
+            self._stored.resource_checked = True
 
-        except ModelError as e:
-            logger.debug(
-                f"no resource was supplied installing from snap store: {e}")
-
-        if self._resource_path:
-            self._is_tar = tarfile.is_tarfile(self._resource_path)
-
-        if self._is_tar:
-            self.slurm_resource = SlurmTarManager(
-                component,
-                self._resource_path
-            )
+        if self._stored.resource_path is not None:
+            resource_size = Path(self._stored.resource_path).stat().st_size
+            if resource_size > 0:
+                if tarfile.is_tarfile(self._stored.resource_path):
+                    self._slurm_resource_manager = SlurmTarManager(
+                        self._slurm_component,
+                        self._stored.resource_path
+                    )
+                else:
+                    self._slurm_resource_manager = SlurmSnapManager(
+                        self._slurm_component,
+                        self._stored.resource_path
+                    )
+            else:
+                self._slurm_resource_manager = SlurmSnapManager(
+                    self._slurm_component,
+                    self._stored.resource_path
+                )
         else:
-            self.slurm_resource = SlurmSnapManager(
-                component,
-                self._resource_path
+            self._slurm_resource_manager = SlurmSnapManager(
+                self._slurm_component,
+                self._stored.resource_path
             )
 
-    def install(self):
-        """Install Slurm."""
-        self.slurm_resource.install()
+    @property
+    def hostname(self):
+        """Return the hostname."""
+        return self._slurm_resource_manager.hostname
+
+    @property
+    def port(self):
+        """Return the port."""
+        return self._slurm_resource_manager.port
+
+    @property
+    def inventory(self) -> str:
+        """Return the node inventory and gpu count."""
+        return get_inventory()
+
+    @property
+    def slurm_installed(self) -> bool:
+        """Return the bool from the stored state."""
+        return self._stored.slurm_installed
 
     def get_munge_key(self) -> str:
-        """Read, encode, decode and return the munge key."""
-        munge_key = self.slurm_resource.munge_key_path.read_bytes()
-        return b64encode(munge_key).decode()
+        """Return the munge key."""
+        return self._slurm_resource_manager.get_munge_key()
+
+    def install(self) -> None:
+        """Prepare the system for slurm."""
+        self._slurm_resource_manager.setup_system()
+        self._stored.slurm_installed = True
+
+    def upgrade(self) -> None:
+        """Upgrade slurm."""
+        self._slurm_resource_manager.upgrade()
 
     def render_config_and_restart(self, slurm_config) -> None:
         """Render the slurm.conf and munge key, restart slurm and munge."""
-        self._write_config(slurm_config)
-        self._write_munge_key_and_restart(slurm_config['munge_key'])
-        is_active = None
-        try:
-            is_active = subprocess.call([
-                "systemctl",
-                "is-active",
-                self.slurm_resource.get_systemd_name(),
-            ]) == 0
-        except subprocess.CalledProcessError as e:
-            logger.error(f"Error running restarting slurm daemon - {e}")
+        if not type(slurm_config) == dict:
+            raise TypeError("Incorrect type for config.")
 
-        if is_active:
-            self._slurm_systemctl("restart")
-        else:
-            self._slurm_systemctl("start")
-        version = self.slurm_resource.get_version()
-        self.charm.unit.set_workload_version(version)
+        # Write munge.key and restart munged.
+        self._slurm_resource_manager.write_munge_key(slurm_config['munge_key'])
+        self._slurm_resource_manager.restart_munged()
+        sleep(1)
 
-    def _slurm_systemctl(self, operation) -> None:
-        """Start systemd services for slurmd."""
-        try:
-            subprocess.call([
-                "systemctl",
-                operation,
-                self.slurm_resource.get_systemd_name(),
-            ])
-        except subprocess.CalledProcessError as e:
-            logger.error(f"Error copying systemd - {e}")
+        # Write slurm.conf and restart the slurm component.
+        self._slurm_resource_manager.write_slurm_config(slurm_config)
+        self._slurm_resource_manager.restart_slurm_component()
+        sleep(1)
 
-    def _write_config(self, context) -> None:
-        """Render the context to a template.
-
-        target: /var/snap/slurm/common/etc/slurm/slurm.conf
-        source: /templates/slurm.conf.tmpl
-        file name can also be slurmdbdb.conf
-        """
-        template_name = self.slurm_resource.get_tmpl_name()
-        target = self.slurm_resource.get_target()
-        ctxt = {**context, **self.slurm_resource.config}
-
-        rendered_template = Environment(
-            loader=FileSystemLoader(str(self._TEMPLATE_DIR))
-        ).get_template(template_name)
-
-        target.write_text(rendered_template.render(ctxt))
-
-    def _write_munge_key_and_restart(self, munge_key) -> None:
-        key = b64decode(munge_key.encode())
-        self.slurm_resource.get_munge_key_path().write_bytes(key)
-
-        try:
-            subprocess.call([
-                "service",
-                self.slurm_resource.munge_sysd,
-                "restart"
-            ])
-        except subprocess.CalledProcessError as e:
-            logger.debug(e)
+        if not self._stored.slurm_version_set:
+            self._charm.unit.set_workload_version(
+                self._slurm_resource_manager.slurm_version
+            )
+            self._stored.slurm_version_set = True
