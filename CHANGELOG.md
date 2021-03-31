d3eeaf539ea1294fdf3fcb2ba2ac4b17a3a25764
omnivector-qa <admin@omnivector.solutions>
2021-03-31 19:05:34 +0000

    release: 0.2.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 0.2.0
+
+- No documented changes.
+
 ## 0.1.1
 
 - No documented changes.
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 
-__version__ = "0.1.1+dev"
+__version__ = "0.2.0"
 
 setup(
     name='slurm-ops-manager',
